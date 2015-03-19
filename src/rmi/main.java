1049c6bf23bba888d76f5ac6a086837258a42508
dufaux <johan.dufaux@etudiant.univ-lille1.fr>
2015-03-19 11:48:17 +0100

    tests

--- src/rmi/main.java
+++ src/rmi/main.java
@@ -1,13 +1,19 @@
 package rmi;
 
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.UnknownHostException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 
 public class main {
 
 	public static void main(String[] args) throws RemoteException {
+		LocateRegistry.createRegistry(1099);
+		
+		
 		ElectionParticipantSynchrone eps1 = new ParticipantSynchroneImpl(65);
 		/*ElectionParticipantSynchrone eps2 = new ParticipantSynchroneImpl(66);
 		ElectionParticipantSynchrone eps3 = new ParticipantSynchroneImpl(10);
@@ -18,10 +24,22 @@ public class main {
 		r.init();*/
 		
 		try {
-			Naming.bind("participant1",eps1);
+			String adresse = InetAddress.getByName("a11p2").toString();
+			System.out.println(adresse);
+			System.out.println(InetAddress.getByName("a11p2").getHostAddress());
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			String url = "rmi://" + InetAddress.getLocalHost().getHostAddress() + "/TestRMI";
+			System.out.println("Enregistrement de l'objet avec l'url : " + url);
+			Naming.rebind(url, eps1);
+			//Naming.bind("participant1",eps1);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
-		} catch (AlreadyBoundException e) {
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 	}
